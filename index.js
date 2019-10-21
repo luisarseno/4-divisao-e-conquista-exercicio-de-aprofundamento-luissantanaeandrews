41416328869e2fb5af09922ac28eab01fc9c1e12
Andrews Souza <andrews.souza@cwi.com.br>
2019-10-21 20:32:15 -0300

    2 e 3

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-function mergeSort(array){
-  if(array.length == 1){
+function mergeSort(array) {
+  if (array.length == 1) {
     return array
   }
 
@@ -9,25 +9,25 @@ function mergeSort(array){
   a = mergeSort(a)
   b = mergeSort(b)
 
-  array = merge(a,b)
+  array = merge(a, b)
 
   return array
 }
 
-function merge(a, b){
+function merge(a, b) {
   const numElements = a.length + b.length
   const mergedArray = []
 
-  for(let i = 0; i < numElements; i++){
-    if(a.length <= 0){
+  for (let i = 0; i < numElements; i++) {
+    if (a.length <= 0) {
       mergedArray.push(...b)
       break
     }
-    if(b.length <= 0){
+    if (b.length <= 0) {
       mergedArray.push(...a)
       break
     }
-    if(a[0] < b[0]){
+    if (a[0] < b[0]) {
       mergedArray.push(a.shift())
     } else {
       mergedArray.push(b.shift())
@@ -37,8 +37,42 @@ function merge(a, b){
   return mergedArray
 }
 
-let arrayTest = [7, 41, 121, 1]
+// let arrayTest = [7, 41, 121, 1]
 
-console.log(arrayTest, 'ARRAY INICIO')
-arrayTest = mergeSort(arrayTest)
-console.log(arrayTest, 'ARRAY FINALL')
+// console.log(arrayTest, 'ARRAY INICIO')
+// arrayTest = mergeSort(arrayTest)
+// console.log(arrayTest, 'ARRAY FINALL')
+
+function maxVal1(a, n) {
+  let max = a[0]
+  for (let i = 1; i < n; i++) {
+    if (a[i] > max) {
+      max = a[i]
+    }
+  }
+  return max
+}
+
+// const arrayTest = [74, 4, 2 ,45, 1, 422, 15]
+// console.log(maxVal1(arrayTest, arrayTest.length))
+
+function maxVal2(a, init, end) {
+  if (end - init <= 1) {
+    return max(a[init], a[end])
+  }
+
+  else {
+    const m = Math.ceil((init + end) / 2)
+    const v1 = maxVal2(a, init, m)
+    const v2 = maxVal2(a, m + 1, end)
+
+    return max(v1, v2)
+  }
+}
+
+function max(v1, v2) {
+  return v1 > v2 ? v1 : v2
+}
+
+// const arrayTest = [5, 55, 4, 21, 91, 47]
+// console.log(maxVal2(arrayTest, 0, arrayTest.length - 1))
